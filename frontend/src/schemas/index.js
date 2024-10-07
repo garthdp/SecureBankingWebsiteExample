46a49e5b6de4714ae100cd70b5ff7126e7fd6ddd
garthdp <garthdp4@gmail.com>
2024-10-07 16:31:40 +0200

    added special character blacklist

--- frontend/src/schemas/index.js
+++ frontend/src/schemas/index.js
@@ -3,12 +3,13 @@ import * as yup from 'yup';
 // Currency regex example for common codes like USD, EUR, GBP
 //const currencyRegex = /^[A-Z]{3}$/; 
 const swiftValidationRegex = /^[A-Z]$/
+const blacklist = /^[a-zA-Z0-9]*$/
 
 export const transactionSchema = yup.object().shape({
     amount: yup.number().positive().integer().required("Amount is required and must be a positive integer"),
-    currency: yup.string().required("Currency is required"),
-    swiftCode: yup.string().required("Please enter swift code").min(8).max(11),
-    recipientName: yup.string().required("Please enter a valid name"),
+    currency: yup.string().matches(blacklist, {message: "No special characters are allowed"}).required("Currency is required"),
+    swiftCode: yup.string().matches(blacklist, {message: "No special characters are allowed"}).required("Please enter swift code").min(8).max(11),
+    recipientName: yup.string().matches(blacklist, {message: "No special characters are allowed"}).required("Please enter a valid name"),
     recipientAccountNumber: yup.number().positive().integer().required("Account number is required and must be a positive integer"),
 });
 
@@ -16,12 +17,12 @@ export const transactionSchema = yup.object().shape({
 const passwordRules = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{5,}$/; // At least 1 digit, 1 lowercase, 1 uppercase, and minimum 5 characters
 
 export const userSchema = yup.object().shape({
-    name: yup.string().required("Name is required"),
-    surname: yup.string().required("Surname is required"),
+    name: yup.string().matches(blacklist, {message: "No special characters are allowed"}).required("Name is required"),
+    surname: yup.string().matches(blacklist, {message: "No special characters are allowed"}).required("Surname is required"),
     email: yup.string().email("Please enter a valid email").required("Email is required"),
     password: yup.string().min(6).matches(passwordRules, { message: "Please make a stronger password" }).required("Password is required"),
-    idNumber: yup.string().required("ID number is required"),
-    accountNumber: yup.string().required("Account number is required")
+    idNumber: yup.string().matches(blacklist, {message: "No special characters are allowed"}).required("ID number is required"),
+    accountNumber: yup.string().matches(blacklist, {message: "No special characters are allowed"}).required("Account number is required")
 });
 
 export const loginSchema = yup.object().shape({
