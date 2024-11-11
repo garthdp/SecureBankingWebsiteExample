1f9c2e18a95699f819bb48239463f8f8f3317e95
Kimberly <104057860+Kimberly2002@users.noreply.github.com>
2024-11-11 14:55:28 +0200

    Christoffel ui fix and logo add

--- frontend/src/pages/welcomepage.js
+++ frontend/src/pages/welcomepage.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom'; // Import Link from react-router-dom
 import './WelcomePage.css'; 
+import logo from '../images/apds_logo.png';
 
 const WelcomePage = () => {
     return (
         
         <div className="welcome-page">
-            
-      <button id="theme-switch"></button>
-            <h1 className="title">Welcome to Your Transactions!</h1>
+            <img src={logo} alt="APDS Logo" className="logo" />
+            <h1 className="title">Welcome to Your Transactions!</h1>            
             <p className="subtitle">Get started by logging in or signing up!</p>
             <div className="button-container">
                 <Link to="/login">
